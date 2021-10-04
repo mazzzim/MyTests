b89a590dad7c7fa2abb07cddcf094d69b43612b5
mazzzim <86350334+mazzzim@users.noreply.github.com>
2021-10-04 15:50:15 +0300

    Add POM

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,13 +14,16 @@ public class Tests {
     public static WebDriver driver;
     public CheckBoxPage checkboxPage;
     public DynamicElementPage dynamicElementPage;
+    public LoginPage loginPage;
+    public DynamicLoadingPage dynamicLoadingPage;
+    public DownloadFilePage downloadFilePage;
 
     @BeforeAll
     public static void createAndStartService() {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver");
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
     }
@@ -33,10 +37,12 @@ public class Tests {
         checkboxPage.clickCheckBox1();
         assertEquals(checkboxPage.checkBox1.isSelected() , checkboxPage.checkBox2.isSelected());
     }
+
     @Test
     public void dynamicElement(){
         //TO DO add a test which goes to http://the-internet.herokuapp.com/entry_ad URL and closes the pop-up
         //then it clicks `click here.` link and checks `THIS IS A MODAL WINDOW` text is displayed on the page
+        //BugReport #......
         dynamicElementPage = new DynamicElementPage(driver);
         dynamicElementPage.getDynamicElementPage();
         dynamicElementPage.closePopUp();
@@ -44,6 +50,55 @@ public class Tests {
         dynamicElementPage.textIsDisplay();
     }
 
+    @Test
+    public void testInvalidLogin(){
+        //TO DO add a test which goes to http://the-internet.herokuapp.com/login URL and fills in incorrect details
+        //then it clicks `Login` button
+        //Here should be a check on error message is displayed on the page
+        loginPage = new LoginPage(driver);
+        loginPage.getLoginPage();
+        loginPage.usernameField.sendKeys(loginPage.wrongLogin);
+        loginPage.passwordField.sendKeys(loginPage.wrongPassword);
+        loginPage.loginButton.click();
+        loginPage.errorResoult.isDisplayed();
+    }
+
+    @Test
+    public void testValidLogin (){
+        //TO DO add a test which goes to http://the-internet.herokuapp.com/login URL and fills in correct details
+        //then it clicks `Login` button
+        //Here should be a check that user is logged in and a new content is displayed
+        loginPage = new LoginPage(driver);
+        loginPage.getLoginPage();
+        loginPage.usernameField.sendKeys(loginPage.trueLogin);
+        loginPage.passwordField.sendKeys(loginPage.truePassword);
+        loginPage.loginButton.click();
+        loginPage.successResoult.isDisplayed();
+    }
+
+    @Test
+    public void testDynamicLoadingElement(){
+        //TO DO add a test which goes to  http://the-internet.herokuapp.com/dynamic_loading/1 URL and
+        //clicks start button
+        //Here should be a check that Hello World text is displayed
+        dynamicLoadingPage = new DynamicLoadingPage(driver);
+        dynamicLoadingPage.getDynamicLoadingPage();
+        dynamicLoadingPage.startButton.click();
+        dynamicLoadingPage.successLoading();
+    }
+
+    @Test
+    public void testDownloadFile(){
+        //TO DO add a test which goes to http://the-internet.herokuapp.com/jqueryui/menu URL and
+        //goes to Enabled-Download-Excel and clicks it
+        //verify that the file is downloaded
+        downloadFilePage = new DownloadFilePage(driver);
+        downloadFilePage.getDownloadFilePage();
+        downloadFilePage.goEnabled();
+        downloadFilePage.goDownloads();
+        downloadFilePage.goExcel();
+        downloadFilePage.fileDownloadCheck();
+    }
     @AfterAll
     public static void tearDown() {
         driver.quit(); }
